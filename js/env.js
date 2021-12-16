4eef248bbe69814e75c77411d20d60e76367067d
Edén <edenclaudioruiz@gmail.com>
2021-12-16 19:23:05 +0100

    CHORE: created file for variables

---
+++ js/env.js
@@ -0,0 +1,18 @@
+const RUNE_SETTINGS = {
+    0: {
+        color: 'green',
+        text: '8'
+    },
+    1: {
+        color: 'orange',
+        text: 'N'
+    },
+    2: {
+        color: 'purple',
+        text: 'K'
+    },
+    3: {
+        color: 'blue',
+        text: 'R'
+    },
+}
