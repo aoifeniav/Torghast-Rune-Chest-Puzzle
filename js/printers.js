442e6dbe44fc91bc3ca0f9160ad50ac53627e25d
Edén <edenclaudioruiz@gmail.com>
2021-12-17 09:20:02 +0100

    REFACTOR: improved puzzle functions

--- js/printers.js
+++ js/printers.js
@@ -21,7 +21,7 @@ function createHandleElements(handles) {
         const chain = createChain(handle);
         const rune = createRune(handle);
         const skull = createSkull(handle, handles);
-    
+
         handleElements.push(chain, rune, skull);
 
         // Adds each handle group to an array with all 4 handles.
@@ -69,34 +69,49 @@ function createRune(handle) {
 function createSkull(handle, handles) {
     const skull = createElement('i', ['fa-skull', 'fas']);
     skull.addEventListener('click', function () {
-        changeRunes(handle.changesCount, handles);
+        activatePuzzle(handle.changesCount, handles);
     });
 
     return skull;
 }
 
 /**
- * Changes the runes according to the clicked skull's changesCount.
+ * Changes the runes according to the clicked skull's changesCount and then changes the chains.
  * @param {number} clickedSkullChangesCount 
  * @param {Array} handles
  */
-function changeRunes(clickedSkullChangesCount, handles) {
+function activatePuzzle(clickedSkullChangesCount, handles) {
     for (let handle of handles) {
         if (clickedSkullChangesCount <= handle.changesCount) {
             handle.color = (++handle.color) % 4;
         }
 
-        const chain = document.getElementById(`chain_${handle.unlockColor}`);
-        if (handle.isUnlocked()) {
-            chain.classList.remove('fa-link');
-            chain.classList.add('fa-unlink');
-        } else {
-            chain.classList.remove('fa-unlink');
-            chain.classList.add('fa-link');
-        }
+        changeRune(handle);
+        changeChain(handle);
+    }
+}
 
-        const rune = document.getElementById(`rune_${handle.unlockColor}`);
-        rune.classList = `rune ${RUNE_SETTINGS[handle.color].color}`;
-        rune.innerText = RUNE_SETTINGS[handle.color].text;
+/**
+ * Changes the rune according to color value.
+ * @param {Object} handle 
+ */
+function changeRune(handle) {
+    const rune = document.getElementById(`rune_${handle.unlockColor}`);
+    rune.classList = `rune ${RUNE_SETTINGS[handle.color].color}`;
+    rune.innerText = RUNE_SETTINGS[handle.color].text;
+}
+
+/**
+ * Changes the chain depending on handle.isUnlocked.
+ * @param {Object} handle 
+ */
+function changeChain(handle) {
+    const chain = document.getElementById(`chain_${handle.unlockColor}`);
+    if (handle.isUnlocked()) {
+        chain.classList.remove('fa-link');
+        chain.classList.add('fa-unlink');
+    } else {
+        chain.classList.remove('fa-unlink');
+        chain.classList.add('fa-link');
     }
 }
